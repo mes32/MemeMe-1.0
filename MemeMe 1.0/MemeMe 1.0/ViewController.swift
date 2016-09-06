32ca1d6f0bb95fdae3393fa317f334be40ec84db
mes32 <stockman.mike@gmail.com>
2016-09-05 23:26:09 -0400

    feature: Add TextFields and top and bottom of screen. The behavior still needs some work.

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textFieldTop: UITextField!
+    @IBOutlet weak var textFieldBottom: UITextField!
+    
+    var isEmptyTop: Bool = true
+    var isEmptyBottom: Bool = true
+    
+    let startingTextTop = "TOP TEXT"
+    let startingTextBottom = "BOTTOM TEXT"
+    let memeTextAttributes = [
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 36)!,
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSStrokeWidthAttributeName : -4.0
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textFieldTop.text = startingTextTop
+        textFieldTop.delegate = self
+        textFieldTop.defaultTextAttributes = memeTextAttributes
+        textFieldTop.textAlignment = .Center
+        
+        textFieldBottom.text = startingTextBottom
+        textFieldBottom.delegate = self
+        textFieldBottom.defaultTextAttributes = memeTextAttributes
+        textFieldBottom.textAlignment = .Center
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
     }
-
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if (textField.text == "") {
+            if (textField == textFieldTop) {
+                textField.text = startingTextTop
+            } else if (textField == textFieldBottom) {
+                textField.text = startingTextBottom
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
 }
 
