bb4418850fd796ec552e035c3205a91d42538e98
mes32 <stockman.mike@gmail.com>
2016-09-08 01:23:17 -0400

    feature: Add functions defined in course notes such as save()

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct Meme {
+        var image: UIImage
+    }
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func save(sender: AnyObject) {
+        //Create the meme
+        let memedImage = generateMemedImage()
+        let meme = Meme( text: textField.text!, image:
+            imageView.image, memedImage: memedImage)
+    }
 
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame,
+                                     afterScreenUpdates: true)
+        let memedImage : UIImage =
+            UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // TODO:  Show toolbar and navbar       
+        
+        return memedImage
+    }
+    
 }
 
